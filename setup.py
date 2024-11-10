ee1d6c9201626b9b791a887e675c05b1cb1c0eb8
sugarkwork <sugarkwork@sugar-knight.com>
2024-11-11 00:38:45 +0900

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with codecs.open('requirements.txt', 'r', encoding='utf-16') as f:
 
 setuptools.setup(
     name="PersistentMemory",
-    version="0.1",
+    version="0.1.1",
     install_requires=requirements,
     packages=setuptools.find_packages(),
     description="Persisten Memory",
