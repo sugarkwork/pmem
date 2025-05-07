5608416e0a99c9e1786c3d578669bb1f4a96a4e1
sugarkwork <sugarkwork@sugar-knight.com>
2025-05-07 17:40:03 +0900

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="skpmem",
-    version="0.1.4",
+    version="0.1.5",
     install_requires=["aiosqlite", "setuptools"],
     packages=setuptools.find_packages(),
     description="Persistent Memory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="sugarkwork",
+    url="https://github.com/sugarkwork/pmem",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
     python_requires='>=3.10',
 )
